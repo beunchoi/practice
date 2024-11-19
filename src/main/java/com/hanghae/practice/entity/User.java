e215ad3b8bde4590001e6bf700f31abb74f1bce6
beunchoi <byungeun0228@gmail.com>
2024-11-20 01:50:09 +0900

    feat : 유저 권한 추가

--- src/main/java/com/hanghae/practice/entity/User.java
+++ src/main/java/com/hanghae/practice/entity/User.java
@@ -2,6 +2,8 @@ package com.hanghae.practice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,5 +40,9 @@ public class User {
 
   @Column(nullable = false)
   private String profile = "자기소개 해주세요";
+
+  @Column(nullable = false)
+  @Enumerated(value = EnumType.STRING)
+  private UserRoleEnum role;
 
 }
