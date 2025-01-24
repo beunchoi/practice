25f8300700a30c07f2947fce06fc9637086c9a09
beunchoi <byungeun0228@gmail.com>
2025-01-24 17:17:11 +0900

    feat : 동영상 재생 테스트 구현

--- src/test/java/com/hanghae/practice/service/VideoServiceImplTest.java
+++ src/test/java/com/hanghae/practice/service/VideoServiceImplTest.java
@@ -1,15 +1,18 @@
 package com.hanghae.practice.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
+import com.hanghae.practice.dto.PlaybackResponseDto;
 import com.hanghae.practice.dto.VideoRequestDto;
 import com.hanghae.practice.dto.VideoResponseDto;
+import com.hanghae.practice.entity.Playback;
 import com.hanghae.practice.entity.Video;
 import com.hanghae.practice.repository.PlaybackRepository;
 import com.hanghae.practice.repository.VideoRepository;
+import java.util.Optional;
 import java.util.UUID;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,10 +29,6 @@ class VideoServiceImplTest {
   private PlaybackRepository playbackRepository;
   @InjectMocks
   private VideoServiceImpl videoService;
-//  @BeforeEach
-//  void beforeEach() {
-//    videoService = new VideoServiceImpl(videoRepository, playbackRepository);
-//  }
 
   @Test
   @DisplayName("동영상 저장")
@@ -42,16 +41,46 @@ class VideoServiceImplTest {
     // when
     VideoResponseDto responseDto = videoService.addVideo(videoRequestDto);
     // then
-    Assertions.assertThat(responseDto.getVideoId()).isEqualTo(videoId);
+    assertThat(responseDto.getVideoId()).isEqualTo(videoId);
   }
 
   @Test
-  void playVideo() {
+  @DisplayName("재생 기록이 있는 경우의 동영상 재생")
+  void existingPlaybackPlayVideo() {
     // given
+    String videoId = "123-456";
+    String userId = "234-567";
+    VideoRequestDto videoRequestDto = new VideoRequestDto("제목", userId, 10000);
+    Video video = new Video(videoId, videoRequestDto);
+    Playback existingPlayback = new Playback(videoId, userId, 10000);
+
+    when(videoRepository.findByVideoId(videoId)).thenReturn(Optional.of(video));
+    when(playbackRepository.findByVideoIdAndUserId(videoId, userId)).thenReturn(Optional.of(existingPlayback));
 
     // when
-
+    PlaybackResponseDto responseDto = videoService.playVideo(videoId, userId);
     // then
+    assertThat(responseDto.getVideoId()).isEqualTo(videoId);
+  }
+
+  @Test
+  @DisplayName("재생 기록이 없는 경우의 동영상 재생")
+  void newPlaybackPlayVideo() {
+    // given
+    String videoId = "123-456";
+    String userId = "234-567";
+    VideoRequestDto videoRequestDto = new VideoRequestDto("제목", userId, 10000);
+    Video video = new Video(videoId, videoRequestDto);
+    Playback newPlayback = new Playback(videoId, userId, 10000);
+
+    when(videoRepository.findByVideoId(videoId)).thenReturn(Optional.of(video));
+    when(playbackRepository.findByVideoIdAndUserId(videoId, userId)).thenReturn(Optional.empty());
+    when(playbackRepository.save(any(Playback.class))).thenReturn(newPlayback);
+
+    // when
+    PlaybackResponseDto responseDto = videoService.playVideo(videoId, userId);
+    // then
+    assertThat(responseDto.getVideoId()).isEqualTo(videoId);
   }
 
   @Test
