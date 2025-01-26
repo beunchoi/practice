a5d3ed13df8ea2b930f0180de5542a9a89bb95b7
beunchoi <byungeun0228@gmail.com>
2025-01-26 22:36:50 +0900

    feat : gateway 서비스 추가

---
+++ gateway-service/src/test/java/com/practice/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.practice.gatewayservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GatewayServiceApplicationTests {
+//
+//  @Test
+//  void contextLoads() {
+//  }
+//
+//}
