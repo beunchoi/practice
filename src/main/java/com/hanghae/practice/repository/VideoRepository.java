89400ba4336a0a97141ecbf1618a8332c5b09103
beunchoi <byungeun0228@gmail.com>
2025-01-26 16:13:42 +0900

    refactor : 동영상 서비스 분리

--- src/main/java/com/hanghae/practice/repository/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hanghae.practice.repository;
-
-import com.hanghae.practice.entity.Video;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video, Long> {
-  Optional<Video> findByVideoId(String videoId);
-}
